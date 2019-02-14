68f8d8415be779ac3773d6f73622001b89e8a96a
RudreshAmity <rudresh.narwal@gmail.com>
2019-02-14 12:50:01 +0530

    Added Node.js

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,32 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin","*"); // Allow which domain are allow to access our resources 
+    res.setHeader(
+        "Access-Control-Allow-Headers", 
+        "Origin, X-Requested-With, Content-Type, Accept"// the incoming request may have these extra header
+        );
+    res.setHeader(
+        "Access-Control-Allow-Methods",     // Here we control which http verb we will use 
+        "GET, POST, PATCH, DELETE, OPTIONS"
+        );    
+    next();
+});
+
+app.post("/api/posts", (req, res, next) => {
+    const post = req.body;
+    console.log(post);
+    res.status(201).json({
+        message: 'Post added sucessfully'
+    })  //201 mena everything is ok and a resource is added
+    
+});
+// we can also use app.get() in below mentioned function
 app.use('/api/posts', (req, res, next) => {
     const posts = [
         {
