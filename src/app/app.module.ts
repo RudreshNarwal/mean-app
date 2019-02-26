b9bd5f720d84fee0a5e238a5729d2fbc183b2925
RudreshAmity <rudresh.narwal@gmail.com>
2019-02-27 04:06:45 +0530

    Mime Type  Validator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import {
    MatInputModule,
    MatToolbarModule,
@@ -9,10 +9,10 @@ import {
    MatButtonModule,
    MatProgressSpinnerModule 
   } from '@angular/material'
-import {MatExpansionModule} from '@angular/material/expansion'; 
+import { MatExpansionModule } from '@angular/material/expansion'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {PostCreateComponent} from './posts/post-create/post-create.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -27,7 +27,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule,
+    ReactiveFormsModule,
     MatToolbarModule,
     MatInputModule,
     MatCardModule,
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,9 +1,10 @@
 // If subscribe is used with http client then we don't have to unsubscribe it as angular take care for that.
 import { Component, OnInit } from "@angular/core";
-import { NgForm } from "@angular/forms";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { PostService } from '../post.service'
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Post } from '../post.modal';
+import { mimeType } from './mime-type.validator'
 @Component({
   selector: "app-post-create",
   templateUrl: "./post-create.component.html",
@@ -13,6 +14,8 @@ export class PostCreateComponent implements OnInit{
   enteredTitle = "";
   enteredContent = "";
   isLoading = false;
+  form: FormGroup;
+  imagePreview: string;
   private mode = 'create';
   private postId: string;
   public post: Post;
@@ -21,6 +24,18 @@ export class PostCreateComponent implements OnInit{
   constructor(public postsService: PostService, public route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl(null, {
+        validators: [Validators.required, Validators.minLength(3)]
+      }),
+      content: new FormControl(null, {
+        validators: [Validators.required]
+      }),
+      image: new FormControl(null, {
+        validators: [Validators.required],
+        asyncValidators: [mimeType]
+      }) 
+    }); //FormGroup constructor take JS object
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if(paramMap.has('postId')){
         this.mode = 'edit';
@@ -30,6 +45,10 @@ export class PostCreateComponent implements OnInit{
           this.isLoading = false; // terminating spinner 
           this.post = {id: postData._id, title: postData.title, content: postData.content};
         });
+        this.form.setValue({  // this setvalue is used only in reactive form only
+          title: this.post.title,
+          content: this.post.content
+        }); //here we are initializing our post in case we got a loaded post
       } else {
         this.mode = 'create';
         this.postId = null;
@@ -38,20 +57,35 @@ export class PostCreateComponent implements OnInit{
     // with this we can listen to changes in routes 
   }
 
-  onSavePost(form: NgForm) {
-    if (form.invalid) {
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];// this does simple type conversion // targeting 1st file 
+    this.form.patchValue({ image: file });// storing file object  // patchValue allow you to target single value
+    this.form.get("image").updateValueAndValidity(); // this inform angular that i modify the input so check it's validity
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result as string;
+    };
+    reader.readAsDataURL(file);
+    // console.log(file);
+    // console.log(this.form);
+  }
+
+
+  onSavePost() {
+    if (this.form.invalid) {
       return;
     }
     this.isLoading = true; // we don't need to change it's value to false as we will navigate away from this page anyway and when we begin, it's initial value will be false
     if (this.mode == 'create') {
-      this.postsService.addPost(form.value.title, form.value.content);
+      this.postsService.addPost(this.form.value.title, this.form.value.content);
     } else {
       this.postsService.updatePost(
         this.postId,
-        form.value.title,
-        form.value.content
+        this.form.value.title,
+        this.form.value.content
       );
     }
-    form.resetForm();
+  //  this.form.resetForm();
+  this.form.reset();
   }
 }
